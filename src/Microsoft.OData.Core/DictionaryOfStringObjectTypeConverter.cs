729a6f297a24b0fefbcd0016689c3a3a22f25750
Brecht Debaere <Brecht.Debaere@spray.com>
2021-03-02 15:04:08 +0100

    Enable array inside dictionary inside dictionary.

--- src/Microsoft.OData.Core/DictionaryOfStringObjectTypeConverter.cs
+++ src/Microsoft.OData.Core/DictionaryOfStringObjectTypeConverter.cs
@@ -1,5 +1,5 @@
 ﻿//---------------------------------------------------------------------
-// <copyright file="DictionaryOfStringObjectTypeConverter.cs" company="Microsoft">
+// <copyright file="DictionaryStringObjectTypeConverter.cs" company="Microsoft">
 //      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 // </copyright>
 //---------------------------------------------------------------------
@@ -16,7 +16,7 @@ namespace Microsoft.OData
     /// <summary>
     /// Handles serialization and deserialization for types derived from Geography.
     /// </summary>
-    internal sealed class DictionaryOfStringObjectTypeConverter : IPrimitiveTypeConverter
+    internal sealed class DictionaryStringObjectTypeConverter : IPrimitiveTypeConverter
     {
         /// <summary>
         /// Write the Atom representation of an instance of a primitive type to an XmlWriter.
@@ -45,65 +45,85 @@ namespace Microsoft.OData
         /// <param name="jsonWriter">Instance of JsonWriter.</param>
         public void WriteJsonLight(object instance, IJsonWriter jsonWriter)
         {
-            var dictionary = (IDictionary<string, object>)instance;
-
-            jsonWriter.StartObjectScope();
-
-            foreach (var item in dictionary)
+            switch (instance)
             {
-                jsonWriter.WriteName(item.Key);
+                case bool boolValue:
+                    jsonWriter.WriteValue(boolValue);
+                    return;
+                case float floatValue:
+                    jsonWriter.WriteValue(floatValue);
+                    return;
+                case short shortValue:
+                    jsonWriter.WriteValue(shortValue);
+                    return;
+                case long longValue:
+                    jsonWriter.WriteValue(longValue);
+                    return;
+                case double doubleValue:
+                    jsonWriter.WriteValue(doubleValue);
+                    return;
+                case Guid guidValue:
+                    jsonWriter.WriteValue(guidValue);
+                    return;
+                case decimal decimalValue:
+                    jsonWriter.WriteValue(decimalValue);
+                    return;
+                case DateTimeOffset dateTimeOffsetValue:
+                    jsonWriter.WriteValue(dateTimeOffsetValue);
+                    return;
+                case TimeSpan timeSpanValue:
+                    jsonWriter.WriteValue(timeSpanValue);
+                    return;
+                case byte byteValue:
+                    jsonWriter.WriteValue(byteValue);
+                    return;
+                case sbyte sbyteValue:
+                    jsonWriter.WriteValue(sbyteValue);
+                    return;
+                case byte[] byteArrayValue:
+                    jsonWriter.WriteValue(byteArrayValue);
+                    return;
+                case int intValue:
+                    jsonWriter.WriteValue(intValue);
+                    return;
+                case string stringValue:
+                    jsonWriter.WriteValue(stringValue);
+                    return;
+            }
 
-                switch (item.Value)
+            var type = instance.GetType();
+#if NETSTANDARD1_1
+            if (GetTypeInfo().IsGenericType)
+#else
+            if (type.IsGenericType)
+#endif
+            {
+                var isDictionary = type.GetGenericTypeDefinition() == typeof(Dictionary<,>);
+                if (isDictionary)
                 {
-                    case bool boolValue:
-                        jsonWriter.WriteValue(boolValue);
-                        break;
-                    case float floatValue:
-                        jsonWriter.WriteValue(floatValue);
-                        break;
-                    case short shortValue:
-                        jsonWriter.WriteValue(shortValue);
-                        break;
-                    case long longValue:
-                        jsonWriter.WriteValue(longValue);
-                        break;
-                    case double doubleValue:
-                        jsonWriter.WriteValue(doubleValue);
-                        break;
-                    case Guid guidValue:
-                        jsonWriter.WriteValue(guidValue);
-                        break;
-                    case decimal decimalValue:
-                        jsonWriter.WriteValue(decimalValue);
-                        break;
-                    case DateTimeOffset dateTimeOffsetValue:
-                        jsonWriter.WriteValue(dateTimeOffsetValue);
-                        break;
-                    case TimeSpan timeSpanValue:
-                        jsonWriter.WriteValue(timeSpanValue);
-                        break;
-                    case byte byteValue:
-                        jsonWriter.WriteValue(byteValue);
-                        break;
-                    case sbyte sbyteValue:
-                        jsonWriter.WriteValue(sbyteValue);
-                        break;
-                    case byte[] byteArrayValue:
-                        jsonWriter.WriteValue(byteArrayValue);
-                        break;
-                    case int intValue:
-                        jsonWriter.WriteValue(intValue);
-                        break;
-                    case string stringValue:
-                        jsonWriter.WriteValue(stringValue);
-                        break;
-                    case IDictionary<string, object> dictionaryOfStringObjectValue:
-                        WriteJsonLight(dictionaryOfStringObjectValue, jsonWriter);
-                        break;
+                    var enumerable = instance as System.Collections.IEnumerable;
+                    jsonWriter.StartObjectScope();
+                    foreach (KeyValuePair<string, object> item in enumerable)
+                    {
+                        jsonWriter.WriteName(item.Key);
+                        WriteJsonLight(item.Value, jsonWriter);
+                    }
+                    jsonWriter.EndObjectScope();
+
+                    return;
                 }
             }
 
-            jsonWriter.EndObjectScope();
+            if (typeof(System.Collections.IEnumerable).IsAssignableFrom(type))
+            {
+                var enumerable = instance as System.Collections.IEnumerable;
+                jsonWriter.StartArrayScope();
+                foreach (var item in enumerable)
+                {
+                    WriteJsonLight(item, jsonWriter);
+                }
+                jsonWriter.EndArrayScope();
+            }
         }
     }
 }
