49214728f9abecd1b99c34d7c80574526305dd02
Garrett DeBruin <corranrogue9@gmail.com>
2022-04-22 10:30:54 -0700

    asdf

--- a.ps1
+++ a.ps1
@@ -1,2 +1,6 @@
 # Get-ChildItem -Path .\ -Recurse -ErrorAction SilentlyContinue -Force
 type msbuild.log
+if (((Get-Content .\build2.txt) -match 'Building target "_CopyOutOfDateSourceItemsToOutputDirectory"').Length -gt 0)
+{
+  exit 1
+}
