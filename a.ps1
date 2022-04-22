e79f44e7f78648d9b095d0512ebc030fb3e4c14c
Garrett DeBruin <corranrogue9@gmail.com>
2022-04-22 11:36:24 -0700

    asdf

--- a.ps1
+++ a.ps1
@@ -2,5 +2,8 @@
 type msbuild.log
 if (((Get-Content .\msbuild.log) -match 'Building target "_CopyOutOfDateSourceItemsToOutputDirectory"').Length -gt 0)
 {
+  Write-Host "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!fail"
   exit 1
 }
+
+Write-Host "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!success"
